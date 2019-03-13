a756bab99fd5a67de83eea448aec55ea5c12e5cd
andy <cddufu@cn.ibm.com>
2019-03-13 14:27:58 +0800

    update application cross origin settings

--- src/main/java/com/mry/config/AppConfig.java
+++ src/main/java/com/mry/config/AppConfig.java
@@ -1,7 +1,6 @@
 package com.mry.config;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,16 +9,12 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class AppConfig {
-	
 	// 设置跨域访问
 	@Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        List<String> origins = new ArrayList<String>();
-        origins.add("*");
-        
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(origins);
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
         corsConfiguration.addAllowedHeader("*"); 
         corsConfiguration.addAllowedMethod("*"); 
         corsConfiguration.setAllowCredentials(true); 
